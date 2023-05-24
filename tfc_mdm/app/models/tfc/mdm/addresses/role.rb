20791c93a208be786db10b04541f9bac6760c4c5
Roberto Vasquez-Angel <rva@beegoodit.de>
2023-05-24 10:43:20 +0200

    Tfc::Mdm fix: missing association

--- tfc_mdm/app/models/tfc/mdm/addresses/role.rb
+++ tfc_mdm/app/models/tfc/mdm/addresses/role.rb
@@ -3,9 +3,14 @@
 module Tfc
   module Mdm
     class Addresses::Role < ApplicationRecord
+      belongs_to :club
       has_many :addresses
 
       validates :identifier, presence: true, uniqueness: true
+
+      def human
+        identifier
+      end
     end
   end
 end
--- tfc_mdm/spec/factories/tfc/mdm/addresses/roles.rb
+++ tfc_mdm/spec/factories/tfc/mdm/addresses/roles.rb
@@ -2,6 +2,7 @@
 
 FactoryBot.define do
   factory :tfc_mdm_addresses_role, class: 'Tfc::Mdm::Addresses::Role' do
+    association(:club, factory: :tfc_mdm_club)
     sequence(:identifier) { |i| "role_#{i}" }
   end
 end
