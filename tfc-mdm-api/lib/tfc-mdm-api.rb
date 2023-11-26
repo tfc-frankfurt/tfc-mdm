796329a75545a66907e364b7c90dbcf0886174a9
Roberto Vasquez Angel <rva@beegoodit.de>
2023-11-26 21:41:04 +0100

    fix(Tfc::Mdm::Api): bundling not possible

--- tfc-mdm-api/lib/tfc-mdm-api.rb
+++ tfc-mdm-api/lib/tfc-mdm-api.rb
@@ -1,8 +1,1 @@
-require "tfc-mdm"
-require "cmor-core-api"
-require "rao-api-resources_controller"
-require "rao-api-service_controller"
-require "rao-query"
-require "responders"
-
 require "tfc/mdm/api"
--- tfc-mdm-api/lib/tfc/mdm/api.rb
+++ tfc-mdm-api/lib/tfc/mdm/api.rb
@@ -1,6 +1,13 @@
-require "tfc/mdm/api/version"
+require "cmor-core-api"
+require "rao-api-resources_controller"
+require "rao-api-service_controller"
+require "rao-query"
+require "responders"
+require "tfc-mdm"
+
 require "tfc/mdm/api/configuration"
 require "tfc/mdm/api/engine"
+require "tfc/mdm/api/version"
 
 module Tfc
   module Mdm
