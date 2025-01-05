7ffe52b3a065702e7d2ccb6bd9d82d09ef42ef56
Roberto Vasquez-Angel <rva@beegoodit.de>
2025-01-05 02:05:46 +0100

    fix(Tfc::Mdm): active record not being loaded lazily

--- tfc-mdm/lib/tfc/mdm/engine.rb
+++ tfc-mdm/lib/tfc/mdm/engine.rb
@@ -10,12 +10,14 @@ module Tfc
       end
 
       config.to_prepare do
-        print "[Tfc::Mdm::Engine] injecting Tfc::Mdm::Model::UserConcern into #{Tfc::Mdm::Configuration.user_class_name}"
-        begin
-          Tfc::Mdm::Configuration.user_class_name.constantize.send(:include, Tfc::Mdm::Model::UserConcern)
-          puts " => OK"
-        rescue NameError => e
-          puts " => [FAILED] #{e.message}"
+        ActiveSupport.on_load(:active_record) do
+          print "[Tfc::Mdm::Engine] injecting Tfc::Mdm::Model::UserConcern into #{Tfc::Mdm::Configuration.user_class_name}"
+          begin
+            Tfc::Mdm::Configuration.user_class_name.constantize.send(:include, Tfc::Mdm::Model::UserConcern)
+            puts " => OK"
+          rescue NameError => e
+            puts " => [FAILED] #{e.message}"
+          end
         end
       end
     end
